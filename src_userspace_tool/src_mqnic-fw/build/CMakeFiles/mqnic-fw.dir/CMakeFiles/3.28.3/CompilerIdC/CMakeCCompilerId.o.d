49be29cedc394aba31c3b47c6aa03c0cc18bb0bd
chenxun233 <158007000+chenxun233@users.noreply.github.com>
2026-01-20 10:12:04 +0800

    deleted unnecessary files

--- src_userspace_tool/src_mqnic-fw/build/CMakeFiles/mqnic-fw.dir/CMakeFiles/3.28.3/CompilerIdC/CMakeCCompilerId.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/mqnic-fw.dir/CMakeFiles/3.28.3/CompilerIdC/CMakeCCompilerId.o: \
- /home/chenxun/Documents/Project/corundum_exanic_x10/src_userspace_tool/src_mqnic-fw/build/CMakeFiles/3.28.3/CompilerIdC/CMakeCCompilerId.c \
- /usr/include/stdc-predef.h
